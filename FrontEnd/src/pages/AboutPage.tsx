63cc472e01525a3595a387d9b812f75455b9081b
kxxnayun <nayun2179@gmail.com>
2026-03-26 13:21:23 +0900

    fix: About 페이지 구조 수정

--- FrontEnd/src/pages/AboutPage.tsx
+++ FrontEnd/src/pages/AboutPage.tsx
@@ -10,52 +10,49 @@ export default function AboutPage() {
           'linear-gradient(to right, rgba(0, 0, 0, 0.08) 1px, transparent 1px), linear-gradient(to bottom, rgba(0, 0, 0, 0.08) 1px, transparent 1px)',
         backgroundSize: '30px 30px',
       }}>
-      <div className="mt-[93px] ml-[60px] flex items-start">
-        <div>
-          <div className="font-grotesk text-[64px] leading-20 font-bold">
-            저는
-            <div className="w-[480px] bg-[#F5D500]">도전을 멈추지 않는</div>
-            개발자입니다.
+      <div className="mt-[93px] ml-[60px] grid grid-cols-[auto_1fr] gap-x-[100px]">
+        <div className="flex flex-col gap-y-[60px] font-mono">
+          <div className="relative ml-[80px] h-[350px] w-[350px]">
+            <div className="absolute right-[-8px] bottom-[-8px] h-full w-full bg-black" />
+            <img
+              src={profile}
+              alt="profile-image"
+              className="absolute top-0 left-0 flex h-full w-full items-center justify-center border-black bg-[#fafafa]"></img>
           </div>
-          <p className="mt-[32px] text-[24px]">
-            함께 일하는 사람들과 좋은 흐름을 만들어 가는 것을 좋아합니다.
-            <br />
-            맡은 일에 최선을 다하고 애정을 가지며 팀 전체가 성장하는 것을 지향합니다.
-          </p>
+          <div>
+            <p className="mt-[30px] text-[22px]">EDUCATION</p>
 
-          <div className="mt-[93px] flex flex-row gap-x-[100px] font-mono">
-            {/* EDUCATION */}
-            <div>
-              <p className="text-[22px]">EDUCATION</p>
-
-              <div className="mt-[23px] flex flex-row text-[20px]">
-                <p className="text-[#777777]">2024</p>
-                <p className="ml-[40px]">Seokyeong university Software Major</p>
-              </div>
-
-              <div className="mt-[23px] flex flex-row text-[20px]">
-                <p className="text-[#777777]">2025</p>
-                <p className="ml-[40px]">9th UMC Web Challenger</p>
-              </div>
-
-              <div className="mt-[23px] flex flex-row text-[20px]">
-                <p className="text-[#777777]">2026</p>
-                <p className="ml-[40px]">10th UMC SKUNIV Leader and Web Challenger</p>
-              </div>
+            <div className="mt-[23px] flex flex-row text-[20px]">
+              <p className="text-[#777777]">2024</p>
+              <p className="ml-[40px]">Seokyeong university Software Major</p>
             </div>
 
-            {/* Certifications */}
-            <div>
-              <p className="text-[22px]">CERTIFICATIONS</p>
+            <div className="mt-[23px] flex flex-row text-[20px]">
+              <p className="text-[#777777]">2025</p>
+              <p className="ml-[40px]">9th UMC Web Challenger</p>
+            </div>
+
+            <div className="mt-[23px] flex flex-row text-[20px]">
+              <p className="text-[#777777]">2026</p>
+              <p className="ml-[40px]">10th UMC SKUNIV Leader and Web Challenger</p>
             </div>
           </div>
         </div>
-        <div className="relative h-[350px] w-[350px]">
-          <div className="absolute right-[-8px] bottom-[-8px] h-full w-full bg-black" />
-          <img
-            src={profile}
-            alt="profile-image"
-            className="absolute top-0 left-0 flex h-full w-full items-center justify-center border-black bg-[#fafafa]"></img>
+
+        <div className="flex flex-col gap-y-[32px]">
+          <div className="font-grotesk text-[64px] leading-20 font-bold">
+            저는
+            <div className="w-[350px] bg-[#F5D500]">도전을 즐기는</div>
+            개발자입니다.
+          </div>
+          <p className="text-[24px]">
+            함께 일하는 사람들과 좋은 흐름을 만들어 가는 것을 좋아합니다.
+            <br />
+            맡은 일에 최선을 다하고 팀과 함께 성장하는 것을 지향합니다.
+          </p>
+          <div className="font-mono">
+            <p className="mt-[64px] text-[22px]">CERTIFICATIONS</p>
+          </div>
         </div>
       </div>
     </div>
